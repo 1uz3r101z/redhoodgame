142b970b2e7ceaf468568836686c38bdf9847bbc
1uz3r101z <mountainwater101z@gmail.com>
2026-03-18 12:22:49 -0500

    My first commit message

---
+++ redhood.py
@@ -0,0 +1,30 @@
+#Fanfic Visual Novel Red Hood x Y/N
+import time 
+import sys
+
+def type_text(text):
+    for letter in text: 
+        sys.stdout.write(letter)
+        sys.stdout.flush()
+        time.sleep(0.05)
+    print()
+
+type_text("``Welcome my Dear , I hope you enjoy this, for this is just for you...``")
+time.sleep(0.05)
+print()
+time.sleep(1)
+
+name = "What is your name Dear?"
+print()
+time.sleep(2)
+
+type_text("Okay, " + name + ", let's get you started into this journey xoxox")
+time.sleep(0.05)
+print()
+print("---------------------------------------------------------------")
+time.sleep(2)
+print()
+
+
+type_text(name + " is in the woods of Gotham resting and getting away from one's issues in a place that belongs to only them.")
+print()
